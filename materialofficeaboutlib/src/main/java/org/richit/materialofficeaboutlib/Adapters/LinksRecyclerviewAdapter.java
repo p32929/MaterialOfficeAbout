b01a1af551c83939f192edfd3b9bfd7b13b73827
p32929 <p32929@yahoo.com>
2019-09-27 23:03:49 +0600

    on click link url

--- materialofficeaboutlib/src/main/java/org/richit/materialofficeaboutlib/Adapters/LinksRecyclerviewAdapter.java
+++ materialofficeaboutlib/src/main/java/org/richit/materialofficeaboutlib/Adapters/LinksRecyclerviewAdapter.java
@@ -7,17 +7,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.squareup.picasso.Picasso;
 
 import org.richit.materialofficeaboutlib.Models.Link;
-import org.richit.materialofficeaboutlib.Models.Member;
 import org.richit.materialofficeaboutlib.R;
 
 import java.util.ArrayList;
@@ -36,13 +35,36 @@ public class LinksRecyclerviewAdapter extends RecyclerView.Adapter<LinksRecycler
     @Override
 
     public LinksRecyclerviewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.office_member, parent, false));
+        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.link_layout, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull LinksRecyclerviewAdapter.ViewHolder holder, int position) {
-        final Link link= linkArrayList.get(position);
+        final Link link = linkArrayList.get(position);
 
+        Picasso.get().load(link.getImageUrl()).fit().into(holder.imageView);
+        holder.textViewName.setText("" + link.getName());
+        holder.textViewUrl.setText("" + link.getUrl());
+
+        holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    openLink(link.getAlt());
+                } catch (Exception e) {
+                    try {
+                        openLink(link.getUrl());
+                    } catch (Exception ex) {
+                        Toast.makeText(context, "Error!!!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+    private void openLink(String urlStr) {
+        Intent browse = new Intent(Intent.ACTION_VIEW, Uri.parse(urlStr));
+        context.startActivity(browse);
     }
 
     @Override
@@ -51,9 +73,17 @@ public class LinksRecyclerviewAdapter extends RecyclerView.Adapter<LinksRecycler
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView textViewName, textViewUrl;
+        RelativeLayout relativeLayout;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            imageView = itemView.findViewById(R.id.faceImage);
+            textViewName = itemView.findViewById(R.id.nameTv);
+            textViewUrl = itemView.findViewById(R.id.descTv);
+            relativeLayout = itemView.findViewById(R.id.parentItemLayout);
         }
     }
 }
